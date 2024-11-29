a1779bafc7385c405aea5afe164b19610d93c92c
mukarif <43062103+mukarif@users.noreply.github.com>
2024-11-29 13:36:48 +0700

    commit show app laravel

---
+++ cooking-project/app/Http/Controllers/LoginController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Session;
+use App\Models\User;
+
+class LoginController extends Controller
+{
+    public function login()
+    {
+        if (Auth::check()) {
+            return redirect('home');
+        }else{
+            return view('login');
+        }
+    }
+
+    public function actionlogin(Request $request)
+    {
+        $data = [
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+        ];
+
+        if (Auth::Attempt($data)) {
+            return redirect('home');
+        }else{
+            Session::flash('error', 'Email atau Password Salah');
+            return redirect('/');
+        }
+    }
+
+    public function actionlogout()
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+    public function register()
+    {
+        return view('register');
+    }
+    
+    public function actionregister(Request $request)
+    {
+        $user = User::create([
+            'email' => $request->email,
+            'username' => $request->username,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+            'active' => 1
+        ]);
+
+        Session::flash('message', 'Register Berhasil. Akun Anda sudah Aktif silahkan Login menggunakan username dan password.');
+        return redirect('register');
+    }
+}
